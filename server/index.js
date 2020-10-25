6ea9bbba2dad57ae0bb84027199f87ac219e20c4
blake-buck <blakemanbuck@gmail.com>
2020-10-25 09:41:32 -0500

    run tests data is no longer hard coded

--- server/index.js
+++ server/index.js
@@ -1,7 +1,49 @@
 async function runTest(url, environment, dimensions, scriptFile){
-    const script = require('./' + scriptFile);
+    const script = require('./scripts/' + scriptFile);
 
     return await script(url, environment, dimensions);
 }
 
-runTest('https://row-board-task.com', 'chromium', {width:1200, height:800}, 'navigateAndScreenshot');
+// runTest('https://row-board-task.com', 'chromium', {width:1200, height:800}, 'navigateAndScreenshot');
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('/storage'));
+
+app.post('/test-script/run', async (req, res) => {
+    const {url, environment, dimensions, scriptFile} = req.body;
+    const results = await runTest(url, environment, dimensions, scriptFile);
+    res.status(200).send({results});
+});
+
+app.post('/test-script/run/collection', async (req, res) => {
+    // console.log(req.body);
+    const {baseUrl, tests, devices} = req.body;
+    let testResults = [];
+
+    for(let i=0; i<tests.length; i++){
+        const test = tests[i];
+        for(let j=0; j< devices.length; j++){
+            const device = devices[j];
+
+            const {path, script} = test;
+            const {width, height, environment} = device;
+            console.log(baseUrl+path);
+            testResults.push(runTest(baseUrl+path, environment, {width:+width, height:+height}, script));
+        }
+    }
+
+    testResults = await Promise.all(testResults);
+
+    console.log('TEST RESULTS');
+    console.log(testResults);
+    res.status(200).send({testResults});
+})
+
+
+
+app.listen(9090, () => console.log('listening on port 9090'));
--- server/scripts/navigateAndScreenshot.js
+++ server/scripts/navigateAndScreenshot.js
@@ -1,6 +1,9 @@
 const playwright = require('playwright');
+const fs = require('fs').promises;
+const path = require('path');
 
 async function navigateAndScreenshot(url, environment, dimensions){
+    console.log(environment)
     const browser = await playwright[environment].launch();
     const context = await browser.newContext();
     const page = await context.newPage();
@@ -11,7 +14,7 @@ async function navigateAndScreenshot(url, environment, dimensions){
     await page.screenshot({ path: `./storage/navigateAndScreenshot/${environment}-${dimensions.width}x${dimensions.height}.png` });
     await browser.close();
 
-    return `/storage/navigateAndScreenshot/${environment}-${dimensions.width}x${dimensions.height}.png`
+    return 'data:image/png;base64,' + await fs.readFile(path.join(__dirname, `../storage/navigateAndScreenshot/${environment}-${dimensions.width}x${dimensions.height}.png`), 'base64')
 }  
 
 module.exports = navigateAndScreenshot;
